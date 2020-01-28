bdbce9ac82a5fe53a561c75016f6e2e6b6f43b28
DionatanLeao <dionatan.ti@outlook.com>
2020-01-28 11:47:56 -0300

    Atualização de usuário com PUT

--- src/main/java/com/mongodb/resource/UserResource.java
+++ src/main/java/com/mongodb/resource/UserResource.java
@@ -4,12 +4,15 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,4 +56,14 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}	
 	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
+
+
