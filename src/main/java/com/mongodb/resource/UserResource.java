78128dddb559807913807177b476a9810c1e411e
DionatanLeao <dionatan.ti@outlook.com>
2020-01-29 10:28:13 -0300

    Referenciando os posts do usuário

--- src/main/java/com/mongodb/resource/UserResource.java
+++ src/main/java/com/mongodb/resource/UserResource.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mongodb.domain.Post;
 import com.mongodb.domain.User;
 import com.mongodb.dto.UserDTO;
 import com.mongodb.services.UserService;
@@ -64,6 +65,11 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
 
 
